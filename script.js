5a09029a0d428b5eab801c85c46bf3514ddfa8f8
leolazz12313 <163777083+leolazz12313@users.noreply.github.com>
2024-03-25 00:20:46 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ function openMenu() {
     const nav = document.querySelector('nav');
     nav.classList.toggle('open');
 }
+
+const sr = ScrollReveal ({
+         distance: '65px',
+         duration: 2600,
+         delay: 450,
+         reset: false
+});
+
+sr.reveal('.left', {delay:200, origin:'top'});
+sr.reveal('.right', {delay:450, origin:'top'});
+sr.reveal('.logo', {delay:550, origin:'left'});
