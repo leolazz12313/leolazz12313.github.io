ba6b624f6269bcb318feec21deaf9ae790cd38b3
Leonardo Lazzareschi <163777083+leolazz12313@users.noreply.github.com>
2024-04-08 19:06:43 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,3 +4,26 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+
+  function openMenu() {
+    const nav = document.querySelector('nav');
+    nav.classList.toggle('open');
+}
+
+const sr = ScrollReveal ({
+         distance: '65px',
+         duration: 2600,
+         delay: 450,
+         reset: true
+});
+
+sr.reveal('.left', {delay:200, origin:'top'});
+sr.reveal('.right', {delay:450, origin:'top'});
+sr.reveal('.logo', {delay:550, origin:'left'});
+
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
